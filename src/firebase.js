ea1ae7bea35f5011e7e42b02cecae2497e330b75
Maria <maria.doguinz@fravega.com.ar>
2026-01-30 15:21:52 -0300

    Update Firebase credentials to US-based project for free Storage

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCZH6wy0loU0t0GaCpI9k4oZ1a3IxrC7ho",
-    authDomain: "sergio-portfolio-9c74d.firebaseapp.com",
-    projectId: "sergio-portfolio-9c74d",
-    storageBucket: "sergio-portfolio-9c74d.firebasestorage.app",
-    messagingSenderId: "476535920332",
-    appId: "1:476535920332:web:b970c8b65277252bafd469",
-    measurementId: "G-D4YFKCDGWM"
+  apiKey: "AIzaSyDw1_XJlfNuw9VKy6zCz6gLDFHEwDTAd68",
+  authDomain: "sergio-portfolio-d74d8.firebaseapp.com",
+  projectId: "sergio-portfolio-d74d8",
+  storageBucket: "sergio-portfolio-d74d8.firebasestorage.app",
+  messagingSenderId: "990850083544",
+  appId: "1:990850083544:web:b5a033e2c7e971e62d85ad"
 };
 
 const app = initializeApp(firebaseConfig);
